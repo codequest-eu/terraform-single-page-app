94b6b9610d8019bdcd04b89ae35601d03576f636
Marek Skrajnowski <marek@codequest.com>
2019-02-18 12:54:13 +0100

    Fix: 404s should be handled by index.html

--- main.tf
+++ main.tf
@@ -96,6 +96,13 @@ resource "aws_cloudfront_distribution" "assets" {
     }
   }
 
+  custom_error_response {
+    error_code            = 404
+    error_caching_min_ttl = 0
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
